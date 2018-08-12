a27e2de42929d06c6f84001f2c62002806ecfd68
Calvin C <lefrogjones@gmail.com>
2018-08-12 18:01:12 -0400

    Fixed font-size changing unhighlighted text

--- js/notepad.js
+++ js/notepad.js
@@ -1,6 +1,9 @@
 loadNotepad();
 
-//font-size is buggy, only works correctly for highlighted text
+/*when no text is highlighted and font size is changed, it encloses new text in a <font size = "7">
+*when font-size is changed again, this event listener ends up getting the old text enclosed in the <font size = "7"> tag 
+*and changes the text to the new font size despite not being highlighted
+*/
 document.getElementById('font-size').addEventListener("change", function(event) {
 	document.execCommand("fontSize", false, "7");
     let fontElements = document.getElementsByTagName("font");
@@ -10,6 +13,19 @@ document.getElementById('font-size').addEventListener("change", function(event) 
         {
             fontElements[i].removeAttribute("size");
             fontElements[i].style.fontSize = `${event.target.value}px`;
+        }
+    }
+})
+
+//as a bandaid fix for the issue in the "font-size" listener above, this should resolve the <font size="7"> tags that slip through
+document.getElementById('notepad').addEventListener("input", function(event) {
+	let fontElements = document.getElementsByTagName("font");
+    for (let i = 0; i < fontElements.length; ++i) 
+    {
+        if (fontElements[i].size == "7") 
+        {
+            fontElements[i].removeAttribute("size");
+            fontElements[i].style.fontSize = `${document.getElementById('font-size').value}px`;
         }
     }
 })
